8353cd789858436dd2d2ab472b9fbb4b97798118
Mark Francisco <mfrancisco581@gmail.com>
2021-02-24 14:13:31 -0600

    Create products controller and /all_products route

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,8 @@
 class Api::ProductsController < ApplicationController
+
+  def list_all_products
+    @products = Product.all
+    render "all_products.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+
+  namespace :api do
+    get "/all_products" => "products#list_all_products"
+  end
 end
