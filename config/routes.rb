36164c11770a0a50e50c06dbab1738abdb1d257a
Mark Francisco <mfrancisco581@gmail.com>
2021-03-08 14:24:49 -0600

    Add user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,10 @@ Rails.application.routes.draw do
     patch "/suppliers/:id/" => "suppliers#update"
     delete "/suppliers/:id" => "suppliers#destroy"
 
+    # USERS
+    post "/users" => "users#create"
+
+
   end
 
 end
