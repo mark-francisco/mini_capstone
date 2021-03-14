ff860db108b38d7a26920f61879fb37da5fcf3f5
Mark Francisco <mfrancisco581@gmail.com>
2021-03-13 18:50:14 -0600

    Add 'old' way of writing has_many :through

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,11 @@ class Category < ApplicationRecord
   has_many :products, :through => :category_products
 
 
+  def products
+    # find ALL of the CategoryProduct instances that belong to this Category
+    products = CategoryProduct.where(:category_id => id).map { |category_product|
+      Product.find_by(:id => category_product.product_id)
+    }
+    
+  end
 end
